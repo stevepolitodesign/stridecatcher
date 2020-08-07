aa1a15b9db60fda29d51bf3d4aaf36376d51059c
Steve Polito Design <stevepolitodesign@users.noreply.github.com>
2020-08-07 06:33:56 -0400

    Configure Travis CI (#2)
    * Add travis config file, and fix failing test.
    * Fix failing test.
    * Run migrations.
    
    Co-authored-by: Steve Polito <info@stevepolitodesign.com>

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
