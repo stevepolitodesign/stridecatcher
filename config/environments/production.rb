2c9645e1811b95dbb52fe1dcb61dcce095f47596
Steve Polito <info@stevepolitodesign.com>
2020-08-08 10:58:24 -0400

    Install and configure devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stridecatcher_production"
 
   config.action_mailer.perform_caching = false
+  
+  # Devise
+  config.action_mailer.default_url_options = { host: 'stridecatcher.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
