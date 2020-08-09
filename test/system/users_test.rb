1a4d8c5a84af2c05a24b9d454f4200bfebc1ab5f
Steve Polito <info@stevepolitodesign.com>
2020-08-09 13:37:43 -0400

    Test user registration and sign in.

---
+++ test/system/users_test.rb
@@ -0,0 +1,29 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  def setup
+    @user = users(:confirmed_user)
+  end
+
+  test "should register" do
+    visit new_user_registration_path
+
+    assert_difference("User.count") do
+      fill_in "Email", with: "unique@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+
+      click_button "Sign up"
+    end    
+  end
+
+  test "should sign in" do
+    visit new_user_session_path
+    
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "password"
+    click_button "Log in"
+
+    assert_selector "p", text: "Signed in successfully."
+  end
+end
