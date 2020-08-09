f21bed81a418b3c4c4f9070604122630bc8365e8
Steve Polito <info@stevepolitodesign.com>
2020-08-09 06:38:28 -0400

    Create User model.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
