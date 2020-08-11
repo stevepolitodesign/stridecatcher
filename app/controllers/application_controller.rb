1c4ee00e503db58ede91a6ba92ecf199bc8b688e
Steve Polito <info@stevepolitodesign.com>
2020-08-10 21:51:23 -0400

    Set application time_zone to current_user.time_zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :set_time_zone, if: :current_user
+
+    private
+
+        def set_time_zone
+            Time.zone = current_user.time_zone unless current_user.time_zone.nil?
+        end
 end
