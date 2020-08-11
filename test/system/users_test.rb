bc899d64c860442c51a6bc7ba76a1f889212886a
Steve Polito <info@stevepolitodesign.com>
2020-08-10 21:37:04 -0400

    Fix brittle tests.

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -9,9 +9,9 @@ class UsersTest < ApplicationSystemTestCase
     visit new_user_registration_path
 
     assert_difference("User.count") do
-      fill_in "Email", with: "unique@example.com"
+      fill_in "Email Address", with: "unique@example.com"
       fill_in "Password", with: "password"
-      fill_in "Password confirmation", with: "password"
+      fill_in "Confirm Password", with: "password"
 
       click_button "Sign up"
     end    
@@ -20,7 +20,7 @@ class UsersTest < ApplicationSystemTestCase
   test "should sign in" do
     visit new_user_session_path
     
-    fill_in "Email", with: @user.email
+    fill_in "Email Address", with: @user.email
     fill_in "Password", with: "password"
     click_button "Log in"
 
