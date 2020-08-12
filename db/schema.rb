9fa07dfe7605f8dc5a70bb9e897b74d2e7872539
Steve Polito <info@stevepolitodesign.com>
2020-08-12 06:58:29 -0400

    Add pace and date to activities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_230609) do
+ActiveRecord::Schema.define(version: 2020_08_12_105702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_08_11_230609) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "pace"
+    t.datetime "date", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
