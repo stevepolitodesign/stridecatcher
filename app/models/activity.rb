6d42e542e1754f72343dff8f1333b8c76f9db1b5
Steve Polito <info@stevepolitodesign.com>
2020-08-12 07:13:33 -0400

    Write simple validations for Activity model.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,6 @@ class Activity < ApplicationRecord
     enum category: [:run, :long_run, :workout, :race, :other]
     enum difficulty: [:easy, :moderate, :hard]
     enum unit: [:miles, :kilometers, :meters, :yards]
+
+    validates :date, presence: true
 end
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:confirmed_user)
+    @activity = @user.activities.build(date: Time.zone.now)
+  end
+
+  test "should be valid" do
+    assert @activity.valid?
+  end
+
+  test "should have date" do
+    @activity.date = nil
+    assert_not @activity.valid?
+  end
 end
