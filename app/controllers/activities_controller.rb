1f7913afac2057667506b3581c21321f00fa8325
Steve Polito <info@stevepolitodesign.com>
2020-08-14 06:00:04 -0400

    Add remaining fields, and create activities#show

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,9 @@
 class ActivitiesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_activity, only: [:show]
+
+    def show
+    end
 
     def new
        @activity = current_user.activities.build
@@ -18,5 +22,9 @@ class ActivitiesController < ApplicationController
 
         def activity_params
             params.require(:activity).permit(:duration, :category, :distance, :difficulty, :unit, :date)
+        end
+
+        def set_activity
+            @activity = Activity.find(params[:id])
         end
 end
