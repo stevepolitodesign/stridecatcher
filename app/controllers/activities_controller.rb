6a6ba5c027a7542250a440d147897583a294f38c
Steve Polito <info@stevepolitodesign.com>
2020-08-14 06:38:52 -0400

    Calculate duration on create.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
     before_action :authenticate_user!
     before_action :set_activity, only: [:show]
+    before_action :calculate_duration, only: [:create]
 
     def show
     end
@@ -26,5 +27,15 @@ class ActivitiesController < ApplicationController
 
         def set_activity
             @activity = Activity.find(params[:id])
+        end
+
+        def calculate_duration
+            calculated_duration = 0
+
+            calculated_duration += params[:activity][:hours].to_i * (60 * 60) if params[:activity][:hours].present?
+            calculated_duration += params[:activity][:minutes].to_i * 60 if params[:activity][:minutes].present?
+            calculated_duration += params[:activity][:seconds].to_i if params[:activity][:seconds].present?
+
+            params[:activity][:duration] = calculated_duration unless calculated_duration == 0
         end
 end
