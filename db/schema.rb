0fd26ef276d2e40bdca737298098b50fd7656d33
Steve Polito <info@stevepolitodesign.com>
2020-08-28 06:27:14 -0400

    Add notified column to shoes table, and create failing test.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_102703) do
+ActiveRecord::Schema.define(version: 2020_08_28_101708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_102703) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.boolean "notified", default: false
     t.index ["user_id"], name: "index_shoes_on_user_id"
   end
 
