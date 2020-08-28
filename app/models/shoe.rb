e8486616621d215d4d44d90c71314a990d94ea8c
Steve Polito <info@stevepolitodesign.com>
2020-08-28 06:54:11 -0400

    Write pseudo code for notification logic.

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -4,4 +4,17 @@ class Shoe < ApplicationRecord
 
     validates :name, presence: true
     validates :allowed_distance_in_miles, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
+
+    after_save :notify_user, if: :user_should_be_notified?
+
+    private
+
+        def notify_user
+            # send email
+        end
+
+        def user_should_be_notified?
+            !self.notified? && self.distance_in_miles >= self.allowed_distance_in_miles
+        end
+
 end
