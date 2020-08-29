d3170036fcaf7a5e0d0b4af2034fbb330a24c145
Steve Polito <info@stevepolitodesign.com>
2020-08-29 14:40:38 -0400

    Account for case where an associated shoe is destroyed.

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe < ApplicationRecord
     belongs_to :user
-    has_many :activities
+    has_many :activities, dependent: :nullify
 
     validates :name, presence: true
     validates :allowed_distance_in_miles, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
